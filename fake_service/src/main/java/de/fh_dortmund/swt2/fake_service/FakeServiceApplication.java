4848f2994051114e70d9499e76ed62306a4c8256
jonas <j.haschke@unnainnrw.de>
2025-04-17 13:31:55 +0200

    setting up final project structure

---
+++ fake_service/src/main/java/de/fh_dortmund/swt2/fake_service/FakeServiceApplication.java
@@ -0,0 +1,13 @@
+package de.fh_dortmund.swt2.fake_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeServiceApplication.class, args);
+	}
+
+}
